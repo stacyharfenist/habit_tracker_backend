097b7c10df1b63aa172592063b77802206c30c6f
Stacy Harfenist <stacy.harfenist@gmail.com>
2018-09-21 15:34:02 -0400

    working sentimentParser

--- server/api/journal.js
+++ server/api/journal.js
@@ -5,8 +5,11 @@ const analyze = require('../sentiment.js')
 module.exports = router;
 
 const sentimentParser = (sent) => {
-    if(sent > 0.4) {
+    console.log('sent', sent)
+    if(sent > 0.2) {
         return 'happy'
+    } else if (sent > 0) {
+        return 'neutral'
     } else {
         return 'sad'
     }
@@ -17,9 +20,9 @@ router.post('/', async (req, res, next) => {
         const dateId = req.body.dateId
         const userId = req.body.userId
         const newEntry = await Journal.create(req.body)
-        console.log('New Entry', newEntry)
         const sentiment = await analyze(newEntry.text)
-        const mood = sentimentParser(sentiment.documentSentiment.magnitude)
+        console.log('SENTIMENT', sentiment)
+        const mood = sentimentParser(sentiment[0].documentSentiment.score)
         const setMood = await MoodOnDate.create({
             dateId: dateId,
             userId: userId,
